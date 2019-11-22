5216f92425fc1b02978d86fbbd351dd5fd7ad8fb
m.veselinov <m.veselinov@edynamix.com>
2019-11-22 08:32:28 +0000

    Add WPF Project with MVVM
    Add WPF user interface project and configures it with Caliburn Micro for our MVVM framwork.
    Also inclused the SimpleContainer Dependency Injection system

---
+++ TRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,54 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TRMDesktopUI.ViewModels;
+
+namespace TRMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer _containter = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        protected override void Configure()
+        {
+            _containter.Instance(_containter);
+
+            _containter
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _containter.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+        protected override object GetInstance(Type service, string key)
+        {
+            return _containter.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _containter.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _containter.BuildUp(instance);
+        }
+    }
+}

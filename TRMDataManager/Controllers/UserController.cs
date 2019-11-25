caf97be91d6443ca0c47ed6d31acc5b6cb21ae42
m.veselinov <m.veselinov@edynamix.com>
2019-11-25 17:00:56 +0000

    Added Db connection string + stored procedure that returns an array of users

---
+++ TRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController 
+    {
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId(); //gets the userId from whoever is logged in
+
+            UserData data = new UserData(); // will be setup with DI
+
+            return data.GetUserById(userId);
+        }
+    }
+}
